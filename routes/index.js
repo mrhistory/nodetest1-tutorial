916d0ee5079d53b39ac02977b40a3d6dc496c258
Jim Chambers <jim.chambers@pamlabdev.com>
2015-07-08 11:44:02 -0400

    Simple node application that reads and writes data from a MongoDB database.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,48 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET helloworld page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(err, docs) {
+    res.render('userlist', {
+      "userlist": docs
+    });
+  });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service. */
+router.post('/adduser', function(req, res) {
+  var db = req.db
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+  var collection = db.get('usercollection');
+
+  collection.insert({
+    "username": userName,
+    "email": userEmail
+  }, function(err, doc) {
+    if (err) {
+      res.send("There was a problem adding the information to the database.");
+    } else {
+      res.redirect("userlist");
+    }
+  });
 });
 
 module.exports = router;
